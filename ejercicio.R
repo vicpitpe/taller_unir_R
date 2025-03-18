2921b7eadee21d38286c03f480a622e91cf446c5
vicpitpe <60582309+vicpitpe@users.noreply.github.com>
2025-03-18 13:38:05 +0100

    Update ejercicio.R

--- ejercicio.R
+++ ejercicio.R
@@ -1,65 +1,48 @@
-# Cargar librerías necesarias
+# Cargar las librerías necesarias
+library(tseries)
 library(ggplot2)
-library(cluster)
 
 # 1. Carga del dataset
-dataset <- read.csv("Mall_Customers.csv")
-head(dataset)
+data("AirPassengers")
 
-# 2. Exploración y limpieza de datos
-dim(dataset)
-str(dataset)
-summary(dataset)
+# Inspeccionar la estructura del dataset
+print(class(AirPassengers))   # Verifica que es una serie temporal (ts)
+print(summary(AirPassengers)) # Resumen estadístico
+print(start(AirPassengers))   # Inicio de la serie
+print(end(AirPassengers))     # Fin de la serie
+print(frequency(AirPassengers)) # Frecuencia: 12 (mensual)
 
-# Renombrar columnas
-names(dataset) <- c("CustomerID", "Genre", "Age", "AnnualIncome", "SpendingScore")
+# 2. Exploración inicial
+# Graficar la serie temporal
+plot(AirPassengers, main="Serie Temporal de AirPassengers", ylab="Número de Pasajeros", xlab="Año")
 
-# Codificar Genre como variable numérica
-dataset$Genre <- ifelse(dataset$Genre == "Male", 1, 0)
+# Calcular estadísticas descriptivas básicas
+mean(AirPassengers)
+sd(AirPassengers)
 
-# Normalizar variables numéricas
-dataset$AnnualIncome <- scale(dataset$AnnualIncome)
-dataset$SpendingScore <- scale(dataset$SpendingScore)
+# 3. Análisis de tendencia y estacionalidad
+# Descomponer la serie temporal
+decomposed <- decompose(AirPassengers)
+plot(decomposed)
 
-# 3. Exploración de variables
-# Histogramas
-ggplot(dataset, aes(x=Age)) + geom_histogram(binwidth=5, fill="blue", color="black")
-ggplot(dataset, aes(x=AnnualIncome)) + geom_histogram(binwidth=5, fill="green", color="black")
-ggplot(dataset, aes(x=SpendingScore)) + geom_histogram(binwidth=5, fill="red", color="black")
+# 4. Análisis de estacionariedad
+# Gráficos de autocorrelación
+acf(AirPassengers, main="ACF de AirPassengers")
+pacf(AirPassengers, main="PACF de AirPassengers")
 
-# Diagramas de caja
-ggplot(dataset, aes(x=factor(Genre), y=Age)) + geom_boxplot()
-ggplot(dataset, aes(x=factor(Genre), y=AnnualIncome)) + geom_boxplot()
-ggplot(dataset, aes(x=factor(Genre), y=SpendingScore)) + geom_boxplot()
+# Prueba de Dickey-Fuller aumentada
+adf.test(AirPassengers)
 
-# 4. Entrenamiento de modelos de clustering
-# K-Means
-set.seed(123)
-wss <- sapply(2:6, function(k) kmeans(dataset[, c("AnnualIncome", "SpendingScore")], k, nstart=10)$tot.withinss)
-plot(2:6, wss, type="b", pch=19, frame=FALSE, xlab="Número de Clusters", ylab="Suma de Cuadrados Interna")
+# Si la serie no es estacionaria, realizar una diferenciación simple
+diff_AirPassengers <- diff(AirPassengers)
+adf.test(diff_AirPassengers)
 
-# Número óptimo de clusters
-optimal_k <- 3
-kmeans_model <- kmeans(dataset[, c("AnnualIncome", "SpendingScore")], optimal_k, nstart=10)
-dataset$KMeansCluster <- kmeans_model$cluster
+# 5. Detección de valores atípicos
+# Visualizar posibles valores atípicos
+boxplot(AirPassengers, main="Boxplot de AirPassengers")
 
-# Clustering jerárquico
-dist_matrix <- dist(dataset[, c("AnnualIncome", "SpendingScore")])
-hclust_model <- hclust(dist_matrix, method="ward.D")
-plot(hclust_model)
-rect.hclust(hclust_model, k=optimal_k, border="red")
-dataset$HierarchicalCluster <- cutree(hclust_model, k=optimal_k)
+# 6. Interpretación de resultados
+# Resumen de tendencias, estacionalidades y ciclos observados
+summary(decomposed)
 
-# 5. Evaluación de modelos
-silhouette_kmeans <- silhouette(kmeans_model$cluster, dist_matrix)
-silhouette_hierarchical <- silhouette(dataset$HierarchicalCluster, dist_matrix)
-mean(silhouette_kmeans[, 3])
-mean(silhouette_hierarchical[, 3])
-
-# 6. Análisis descriptivo de segmentos
-aggregate(dataset[, c("Age", "AnnualIncome", "SpendingScore")], by=list(dataset$KMeansCluster), FUN=mean)
-aggregate(dataset[, c("Age", "AnnualIncome", "SpendingScore")], by=list(dataset$HierarchicalCluster), FUN=mean)
-
-# 7. Visualización
-ggplot(dataset, aes(x=AnnualIncome, y=SpendingScore, color=factor(KMeansCluster))) + geom_point() + labs(title="K-Means Clustering")
-ggplot(dataset, aes(x=AnnualIncome, y=SpendingScore, color=factor(HierarchicalCluster))) + geom_point() + labs(title="Clustering Jerárquico")
+# Guardar el script en un archivo .R o .Rmd y subirlo a GitHub
